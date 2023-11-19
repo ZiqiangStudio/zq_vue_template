ffde344c0aea1ac7c615b36d5dc1eb7d2509daf8
Saltro <2373117747@qq.com>
2023-11-19 21:56:58 +0800

    fix: 修复类型问题

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+
+  // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
+  const vueComponent: DefineComponent<{}, {}, any>;
+
+  export default vueComponent;
+}
