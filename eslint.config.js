68ef300f3aff55cff6c0fe273b13830f96d0a162
WelkinSky <welkinsky@163.com>
2024-10-24 12:54:51 +0800

    fix: 更新版本

---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+export default [
+  {
+    extends: ['plugin:vue/vue3-recommended', 'plugin:prettier/recommended'],
+    files: ['.vue', '.js', '.jsx', '.cjs', '.mjs', '.ts', '.tsx', '.cts', '.mts']
+  }
+];
