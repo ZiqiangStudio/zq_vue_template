2abf99031b71264b6c4892ddcb21979ec48cf954
Saltro <2373117747@qq.com>
2024-11-09 11:36:13 +0800

    feat: 接入@zqstudio/configs

--- eslint.config.js
+++
@@ -1,6 +0,0 @@
-export default [
-  {
-    extends: ['plugin:vue/vue3-recommended', 'plugin:prettier/recommended'],
-    files: ['.vue', '.js', '.jsx', '.cjs', '.mjs', '.ts', '.tsx', '.cts', '.mts']
-  }
-];
---
+++ eslint.config.mjs
@@ -0,0 +1,4 @@
+import { config } from '@zqstudio/configs/eslint';
+export default config({
+  ignores: ['node_modules/**', 'dist/**'],
+});
